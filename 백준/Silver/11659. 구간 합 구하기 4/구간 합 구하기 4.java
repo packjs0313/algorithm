c7ee7f06c3856357d74ff11528e0845d11c2e82b
packjs0313 <147272391+packjs0313@users.noreply.github.com>
2025-04-11 15:35:07 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1852 ms, Memory: 258344 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -8,6 +8,34 @@ public class Main {
         int N = sc.nextInt();
         int M = sc.nextInt();
         int[] num = new int[N];
+        int[] sum = new int[N+1];
+        
+        for(int i =0;i < N; i++){
+            num[i] = sc.nextInt();
+            sum[0]= 0;
+            sum[i+1] = sum[i]+num[i];
+        }
+        
+        for(int i = 0; i < M; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(sum[b]-sum[a-1]);
+        }
+    }
+}
+
+/*요약 전 원래 코드
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] num = new int[N];
         int[] sum = new int[N];
         
         for(int i =0;i < N; i++){
@@ -29,4 +57,4 @@ public class Main {
             }
         }
     }
-}
+}*/
