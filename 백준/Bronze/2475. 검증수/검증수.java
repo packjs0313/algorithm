41576546e6c5cbb65b16e7e1315b78fe259a0f45
packjs0313 <147272391+packjs0313@users.noreply.github.com>
2025-07-04 14:11:34 +0900

    [Bronze V] Title: 검증수, Time: 168 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a[] = new int[5];
+        int sum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+
+            sum += a[i]*a[i];
+        }
+        System.out.println(sum%10);
+    }
+}
