5a81b149d153b29a8c0b1f7c78762811bc23f8a2
packjs0313 <147272391+packjs0313@users.noreply.github.com>
2025-09-26 14:35:19 +0900

    [Bronze II] Title: 거스름돈, Time: 168 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -5,15 +5,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int num = 1000 - sc.nextInt();
-
         int[] arr = {500,100,50,10,5,1};
         int count = 0;
-        int a = 0;
-
-            for (int i : arr) {
-                count += (num/i);
-                num = num%i;
-            }
+        
+        for (int i : arr) {
+            count += (num/i);
+            num = num%i;
+        }
         System.out.println(count);
     }
 }
