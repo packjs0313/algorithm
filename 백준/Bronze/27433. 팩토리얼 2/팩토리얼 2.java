00997218e8d01eb3b057430944af368860eb1c8c
박상진 <147272391+packjs0313@users.noreply.github.com>
2026-01-01 04:13:47 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long result = 1;
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            result *= i;
+        }
+        System.out.println(result);
+    }
+}
