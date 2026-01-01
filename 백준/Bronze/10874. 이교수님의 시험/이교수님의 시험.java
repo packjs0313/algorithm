fd97e4a442d86c8298a90a68d3539e853c970bcb
박상진 <147272391+packjs0313@users.noreply.github.com>
2026-01-01 16:35:00 +0900

    [Bronze III] Title: 이교수님의 시험, Time: 216 ms, Memory: 19740 KB -BaekjoonHub

---
+++ 백준/Bronze/10874. 이교수님의 시험/이교수님의 시험.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int students = sc.nextInt();
+        int[] answer = new int[10];
+        int[][] arr = new int[students][10];
+        int[] reTest = new int[students]; 
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = i % 5 + 1;
+        }
+
+        for (int i = 0; i < students; i++) {
+            for (int j = 0; j < 10; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+            if (Arrays.equals(arr[i],answer)) {
+                reTest[i] = i+1;
+            }
+        }
+        Arrays.sort(reTest);
+    
+        for (int i = 0; i < reTest.length; i++) {
+            if (reTest[i] != 0) {
+                System.out.println(reTest[i]);
+            }
+        }
+    }
+}
