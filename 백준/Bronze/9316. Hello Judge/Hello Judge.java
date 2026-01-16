9ea679c3284c98cc3cdff3d63a1c438bb4cab189
박상진 <147272391+packjs0313@users.noreply.github.com>
2026-01-16 21:04:42 +0900

    [Bronze V] Title: Hello Judge, Time: 188 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tc = sc.nextInt();
+
+        for (int i = 1; i <= tc; i++) {
+            System.out.println("Hello World, Judge "+ i +"!");
+        }
+    }
+}
