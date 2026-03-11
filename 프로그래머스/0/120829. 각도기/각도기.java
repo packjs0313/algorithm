f7a8390a7d549ca885d2678e4421826181533882
박상진 <147272391+packjs0313@users.noreply.github.com>
2026-03-11 09:16:50 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,14 +1,14 @@
 class Solution {
     public int solution(int angle) {
         int answer = 0;
-        if (0 < angle && angle < 90) {
-            answer = 1;
-        } else if (angle == 90) {
-            answer = 2;
-        } else if (90< angle && angle < 180){
-            answer = 3;
-        } else if (angle == 180){
-            answer = 4;
+        if (angle == 180) {
+              answer = 4;
+        }else if (angle > 90) {
+              answer = 3;
+        }else if (angle == 90) {
+              answer = 2;
+        }else {
+              answer = 1;
         }
         return answer;
     }
