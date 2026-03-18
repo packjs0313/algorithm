d773201a6f791aca894769c61ce347bb98f50e96
박상진 <147272391+packjs0313@users.noreply.github.com>
2026-03-18 11:52:47 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,10 +1,7 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 12000 * n + 2000 * k;
-        int ddd = n / 10 * 2000;
-        if (n >= 10) {
-            answer -= ddd;
-        }
+        int answer = n * 12000 + k * 2000 -1 * 2000;
+        answer = n * 12000+ k * 2000-n/10*2000;
         return answer;
     }
 }
