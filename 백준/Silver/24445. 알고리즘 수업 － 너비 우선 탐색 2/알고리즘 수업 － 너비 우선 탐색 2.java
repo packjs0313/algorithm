31655ebc8b822b4aa90163e75e8dfafa38cf841f
박상진 <147272391+packjs0313@users.noreply.github.com>
2026-04-09 19:57:35 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 2256 ms, Memory: 325864 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Main {
+    static boolean[] v;
+    static ArrayList<Integer>[] grp;
+    static int[] result;
+    static int count =0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int st = sc.nextInt();
+
+        v = new  boolean[n+1];
+        grp = new ArrayList[n+1];
+        result = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            grp[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            grp[a].add(b);
+            grp[b].add(a);
+        }
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(grp[i],Collections.reverseOrder());
+        }
+        bfs(st);
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(result[i]);
+        }
+
+    }
+    static void bfs(int now){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(now);
+        v[now] = true;
+
+        while (!q.isEmpty()){
+            int next = q.poll();
+            count++;
+            result[next] = count;
+            for (int a :grp[next]) {
+                if (!v[a]) {
+                    v[a] = true;
+                    q.add(a);
+                }
+            }
+        }
+    }
+}
